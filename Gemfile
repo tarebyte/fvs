aa6688175b13675b2f4b09b3c658af7e67e08591
Mark Tareshawty <tarebyte@gmail.com>
2014-02-09 14:26:58 -0500

    added user authentication and bootswatch

--- Gemfile
+++ Gemfile
@@ -3,13 +3,15 @@ ruby '2.1.0'
 
 gem 'rails', '4.0.2'
 
+gem 'bootstrap-sass'
 gem 'coffee-rails', '~> 4.0.0'
+gem 'devise'
 gem 'jbuilder', '~> 1.2'
 gem 'jquery-rails'
 gem 'pg'
 gem 'pundit'
 gem 'sass-rails', '~> 4.0.0'
-gem 'slim-rails'
+gem 'therubyracer'
 gem 'turbolinks'
 gem 'uglifier', '>= 1.3.0'
 
@@ -18,6 +20,7 @@ group :development, :test do
   gem 'better_errors'
   gem 'pry-plus'
   gem 'pry-rails'
+  gem 'rails_layout'
   gem 'rspec-rails'
   gem 'quiet_assets'
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe User do
+  before do
+    @user = FactoryGirl.create(:user)
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name)  }
+  it { should respond_to(:email) }
+  it { should respond_to(:posts) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+end
