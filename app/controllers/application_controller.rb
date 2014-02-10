f1b32f08109075b283a0809455c9af79d813cdd0
Mark Tareshawty <tarebyte@gmail.com>
2014-02-09 20:48:44 -0500

    added username to signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # After sign in go to the root path
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+
   def after_sign_in_path_for(resource_name)
     root_path
   end
 
   def after_sign_in_path_for(resource)
     listings_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
   end
 end
